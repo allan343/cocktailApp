e2708b7a1facba9316bef95a45359b8ebf7d7898
Allan Cheng <allanhfcheng@gmail.com>
2021-01-21 23:31:37 -0800

    stuck

--- src/AddCockTail/AddCockTail.js
+++ src/AddCockTail/AddCockTail.js
@@ -6,86 +6,82 @@ import ApiContext from '../ApiContext/ApiContext';
 class AddCockTail extends React.Component {
 
   static contextType = ApiContext;
-  
+
   constructor(props) {
     super(props);
-    this.state={
-    cocktailQuery:""
-  };
+    this.state = {
+      cocktailQuery: ""
+    };
   }
 
   updateCockTail(cocktailQuery) {
     console.log(cocktailQuery)
-    this.setState({ cocktailQuery:cocktailQuery });
+    this.setState({ cocktailQuery: cocktailQuery });
   };
-/*
-searchCocktail(cocktailQuery) {
-  console.log(cocktailQuery)
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailQuery}`)
-      .then((cockTailRes) => {
-          if (!cockTailRes.ok)
-              return cockTailRes.json().then(e => Promise.reject(e));
-          return (cockTailRes.json());
-      })
-      .then((cockTailRes) => {
-        console.log(cockTailRes.drinks[0].idDrink)
-          this.setState({ cockTailQuery: cockTailRes });
-         // this.context.addCockTails(cockTailRes)
-      })
-      .catch(error => {
-      });
+  /*
+  searchCocktail(cocktailQuery) {
+    console.log(cocktailQuery)
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailQuery}`)
+        .then((cockTailRes) => {
+            if (!cockTailRes.ok)
+                return cockTailRes.json().then(e => Promise.reject(e));
+            return (cockTailRes.json());
+        })
+        .then((cockTailRes) => {
+          console.log(cockTailRes.drinks[0].idDrink)
+            this.setState({ cockTailQuery: cockTailRes });
+           // this.context.addCockTails(cockTailRes)
+        })
+        .catch(error => {
+        });
+  
+        console.log(this.context.cocktails)
+  }
+  */
+  render() {
+    return (
+      <div>
+        <form className="folder" onSubmit={
+          (event) => {
 
-      console.log(this.context.cocktails)
-}
-*/
-  render(){
-  return (
-    <div>
-         <form className="folder" onSubmit={(event) => {
-
-          event.preventDefault();
-         console.log("submit")
+            event.preventDefault();
+            console.log("submit")
+            /*
+               fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.cocktailQuery}`)
+               .then((cockTailRes) => {
+                   if (!cockTailRes.ok)
+                       return cockTailRes.json().then(e => Promise.reject(e));
+                   return (cockTailRes.json());
+               })
+               .then((cockTailRes) => {
+                 console.log(cockTailRes.drinks[0].idDrink)
+                //   this.setState({ cockTailQuery: cockTailRes });
+                   this.context.addCockTail(cockTailRes)
+               })
+               .catch(error => {
+               });
          
-            fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.cocktailQuery}`)
-            .then((cockTailRes) => {
-                if (!cockTailRes.ok)
-                    return cockTailRes.json().then(e => Promise.reject(e));
-                return (cockTailRes.json());
-            })
-            .then((cockTailRes) => {
-              console.log(cockTailRes.drinks[0].idDrink)
-             //   this.setState({ cockTailQuery: cockTailRes });
-                this.context.addCockTail(cockTailRes)
-            })
-            .catch(error => {
-            });
-      
+               */
+            //add class object to class array
+            // then go back to previous page
+            // this.context.addCockTail(cockTailRes)
+            // this.props.history.goBack();
+          }}>
 
-
-
-
-
-
-
-          //add class object to class array
-          // then go back to previous page
-         // this.context.addCockTail(cockTailRes)
-         // this.props.history.goBack();
-        }}></form>
-
-        
-          <h1>Country List</h1>
-          <input type="text" className="folder__control"
-                name="classname" id="classname"  onChange={e => this.updateCockTail(e.target.value)} required="required" />
-     <button type="submit" className="saveClassButton" >
-                  Save
+          <div>
+          
+            <input type="text" className="folder__control"
+              name="classname" id="classname" onChange={e => this.updateCockTail(e.target.value)} required="required" />
+            <button type="submit" className="saveClassButton" >
+              Save
                 </button>
-    {/*
-    <CountryList countryList={countryList}/>*/}
-        
-        </div>
-       );
-}
+
+
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default AddCockTail;
